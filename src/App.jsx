97c5385f0bcbd00a9f339c3260be87eeedfa478e
The-little-spring <100764083+The-little-spring@users.noreply.github.com>
2023-05-22 12:41:38 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Square from "./components/Square";
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
